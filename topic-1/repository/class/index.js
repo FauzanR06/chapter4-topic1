543e84abad5cc8f090d781c773a4e5b06de51109
FauzanR06 <fauzanm157@gmail.com>
2024-03-27 05:50:23 +0700

    first vommit

---
+++ topic-1/repository/class/index.js
@@ -0,0 +1,44 @@
+const { classes, student } = require("../../models");
+
+exports.getClasses = async () => {
+  const data = await classes.findAll({
+    include: {
+      model: student,
+    },
+  });
+  return data;
+};
+
+exports.getClass = async (id) => {
+  const data = await classes.findAll({
+    where: {
+      id,
+    },
+    include: {
+      model: student,
+    },
+  });
+  if (data.length > 0) {
+    return data[0];
+  }
+  throw new Error("Class is not found!");
+};
+
+exports.createClass = async (payload) => {
+  const data = await classes.create(payload);
+  return data;
+};
+
+exports.updateClass = async (id, payload) => {
+  const data = await classes.update(payload, {
+    where: {
+      id,
+    },
+  });
+  return data;
+};
+
+exports.deleteClass = async (id) => {
+  const data = await classes.destroy({ where: { id } });
+  return null;
+};
